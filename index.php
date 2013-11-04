3896a892534db00a242e613d766b6a2458fca48a
Melanie27 <melaniemcganney@gmail.com>
2013-11-04 10:44:44 -0800

    bootstrap added

--- index.php
+++ index.php
@@ -4,14 +4,17 @@
     <title>OWW Project</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
+    <link href="_css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_css/mystyles.css" rel="stylesheet" media="screen">
    
    
    
   </head>
   <body>
-  	<P>Setting up Oakwood 2</P>
+  	<P>Hello World</P>
   	
   
-  
+  	<script src="_/js/bootstrap.js"></script>
+  	<script src="_/js/myscript.js"></script>
   </body>
 </html>
