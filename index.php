8fd19f857040a35a8bc94178366e0af7ccc28053
Melanie27 <melaniemcganney@gmail.com>
2013-11-05 09:31:45 -0800

    added ie meta tag

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <head>
     <title>OWW Project</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <!-- Bootstrap -->
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
