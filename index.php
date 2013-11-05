50173032c2d1badea3b1b1ce9ba2c6ece83605c4
Melanie27 <melaniemcganney@gmail.com>
2013-11-04 16:59:47 -0800

    News Section

--- index.php
+++ index.php
@@ -4,15 +4,164 @@
     <title>OWW Project</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="_css/bootstrap.css" rel="stylesheet" media="screen">
-    <link href="_css/mystyles.css" rel="stylesheet" media="screen">
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
    
    
    
   </head>
-  <body>
-  	<P>Hello World</P>
-  	
+  <body id="home">
+  	<section class="container landing">
+  		<div class="content">
+  			<div class="wrapper">
+  				
+  				<div class="content row">
+	  				<div class="col-lg-12">
+		  				<header class="clearfix">
+			  				<section id="branding">
+				  				<div class="col-md-3">
+				  					<a href="index.php" id="logo">
+				  						<img src="images/sitewide/logo-oww.jpg" alt="Oakwood Worldwide Logo" class="img-responsive" />
+				  					</a>
+				  				</div>
+				  					<section class="navbar col-md-9">
+					  					<ul class="nav navbar-nav">
+						  					<li><a href="index.php">Our Brands</a></li>
+						  					<li><a href="index.php">Global Corporate Housing</a></li>
+						  					<li><a href="index.php">Industry Insights</a></li>
+						  					<li><a href="index.php">Vision and Leadership</a></li>
+						  					<li class="active"><a href="index.php">News</a></li>
+						  					<li><a href="index.php">Marriott</a></li>
+						  					<li><a href="index.php">Careers</a></li>
+						  				</ul><!-- nav -->
+						  			</section><!-- navbar -->
+				  				
+						  	</section><!-- branding -->						  
+						</header><!-- header -->
+						 
+						<section class="two ">
+							<section class="heading">
+								<div class="container">
+									<h1>News</h1>
+								</div>
+							</section>
+							<div class="container">
+								<section class="hidden-xs header-overlay">
+								
+									<section class="cover-story col-md-4">
+										<img src="images/news/cover-story-tb-01.jpg" alt="cover-story-tb-01" class="img-responsive">
+									</section>
+									<section class="blurb col-md-4">
+										<h2>Story Title Lorem Ipsum Dolore Sit Amet</h2>
+										<P><span class="city">LOS ANGELES</span> - July 31, 2013 - Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor sppo suere lorem ipsum.</P>
+										<p class="full-story"><a href="#">Full Story</a></p>
+									</section>
+									<section class="teasers col-md-4">
+										<ul>
+											<li>
+												<img src="images/news/latest-news-tb-01.jpg" alt="latest-news-tb-01" width="120" height="75" class="pull-left">
+												<p class="date">Oct 5, 2013</p>
+												<p class="headline">Lorem Ipsum Dolore Sit Amet Oakwood</p>
+											</li>
+											<li>
+												<img src="images/news/latest-news-tb-02.jpg" alt="latest-news-tb-01" width="120" height="75" class="pull-left">
+												<p class="date">Oct 5, 2013</p>
+												<p class="headline">Lorem Ipsum Dolore Sit Amet Oakwood</p>
+											</li>
+											<li>
+												<img src="images/news/latest-news-tb-03.jpg" alt="latest-news-tb-01" width="120" height="75" class="pull-left">
+												<p class="date">Oct 5, 2013</p>
+												<p class="headline">Lorem Ipsum Dolore Sit Amet Oakwood</p>
+											</li>
+										</ul>
+									</section>
+								</section>
+							</div>
+						  	
+						</section> <!--two-->
+						
+						
+						 
+					</div><!-- column -->
+				</div><!-- content -->
+				
+				<div class="content row">
+					<div class="col-lg-12">
+						<section class="clearfix news-topics">
+							<section class="three">
+								<div class="container">
+									<section class="col-md-4" id="press-room">
+										<h1>Press Room</h1>
+										<h3>Latest Releases</h3>
+									</section><!--Press Room-->
+									<section class="col-md-4" id="in-the-news">
+										<h1>In the News</h1>
+										<h3>Latest Releases</h3>
+									</section><!--Press Room-->
+									<section class="col-md-4" id="media-relations">
+										<h1>Media Relations</h1>
+										<h3>Latest Releases</h3>
+									</section><!--Press Room-->
+								</div>
+							</section>
+						</section>
+					</div>
+				</div><!-- content -->
+				
+				<div class="content row">
+					<div class="col-lg-12">
+						<section class="four hidden-xs">
+						</section>
+					</div>
+				</div><!--content 4-->
+				
+				<div class="content row">
+					<div class="col-lg-12">
+						<section class="five">
+							<div class="container">
+								<section id="upcoming-events">
+									<div class="col-md-4">
+										<h1>Upcoming Events</h1>
+										
+									</div>
+									<div class="col-md-8">
+										<div class="row">
+											<div class="col-md-6">
+												<h4>Headline Lorem Ipsum Dolore Sit Amet Event</h4>
+											</div>
+											<div class="col-md-6">
+												<h4>Headline Lorem Ipsum Dolore Sit Amet Event</h4>
+											</div>
+											<div class="row">
+												<div class="col-md-4 col-md-offset-4">All 2013 Events</div>
+											</div>
+										</div>
+									</div>
+								</section>
+							</div>
+						</section>
+					</div>
+				</div>
+  				
+  				
+  				<div class="content row">
+					<div class="col-lg-12">
+						<section class="six hidden-xs">
+						</section>
+					</div>
+				</div><!--content 6-->
+				
+				<div class="content row">
+					<div class="col-lg-12">
+						<footer>
+							Footer
+						</footer>
+					</div>
+				</div><!--Footer-->
+
+  			</div><!--wrapper-->
+  		</div><!--End Content-->
+  	</section><!-- container -->
   
   	<script src="_/js/bootstrap.js"></script>
   	<script src="_/js/myscript.js"></script>
